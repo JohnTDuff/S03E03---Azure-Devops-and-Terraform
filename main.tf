2e6b0c2bbfc52aeed91f80f0e82c428bc6f4cf01
John D <johndffaz@outlook.com>
2021-01-22 15:40:05 +0000

    updated pipeline file

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rf_storage"
+        storage_account_name = "tfstorageaccjd"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "UK South"
